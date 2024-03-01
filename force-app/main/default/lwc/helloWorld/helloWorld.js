6f88a7d6f200a8e616d11c23d7907f9b626d9455
Michael Drummond <97915761+MichaelD3289@users.noreply.github.com>
2024-03-01 14:02:23 -0700

    testing external API calls

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,8 +1,17 @@
 import { LightningElement } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
-        greeting = 'World';
+        pokemonDetails;
+        chosenPokemon = 'ditto';
+        endpoint = "https://pokeapi.co/api/v2/pokemon/"
+
         changeHandler(event) {
-        this.greeting = event.target.value;
+        this.chosenPokemon = event.target.value;
+        }
+
+        getPokemonHandler() {
+                fetch(this.endpoint + this.chosenPokemon).then(res => res.json()).then(data => {
+                        this.pokemonDetails = JSON.stringify(data, null, 4)
+                })
         }
 }
